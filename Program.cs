3fb3e36103d7e9ae6aff4568180060ba0657ae9e
FLAVIO CELESTINO <81762067+punk23@users.noreply.github.com>
2021-06-17 19:21:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Descobrindo_triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo x;
+            x = new triangulo();
+            Console.WriteLine("Digite abaixo o valor dos lados do possível triangulo:");
+
+            x.a = double.Parse(Console.ReadLine());
+            x.b = double.Parse(Console.ReadLine());
+            x.c = double.Parse(Console.ReadLine());
+
+            if (x.a < x.b + x.c)
+            {
+                Console.WriteLine("É um triângulo");
+            }
+            else
+            {
+                Console.WriteLine("Não é um triângulo");
+
+            }
+        }
+    }
+}
---
+++ triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Descobrindo_triangulo
+{
+    class triangulo
+    {
+        public double a, b, c;
+
+        public triangulo()
+        {
+        }
+
+        public triangulo(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
